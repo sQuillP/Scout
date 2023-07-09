2127f89858db21fed830923390a039dfab3e0a58
unknown <willpattisons@yahoo.com>
2023-07-08 21:09:57 -0400

    added bug fixes to viewticket.js

--- client/src/pages/project dashboard/pages/ViewTicket.js
+++ client/src/pages/project dashboard/pages/ViewTicket.js
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom"
-import { useState, useEffect } from "react";
+import { useState, useEffect, forwardRef } from "react";
 import useDebounce from "../../../hooks/useDebounce";
 import NoComments from "../components/NoComments";
 import _ from 'lodash';
@@ -17,8 +17,14 @@ import {
     FormControlLabel,
     Box,
     Switch,
-    Modal,
+    Dialog,
     CircularProgress,
+    Slide,
+    DialogTitle,
+    DialogContent,
+    DialogContentText,
+    DialogActions,
+    Button,
 
 } from '@mui/material';
 
@@ -59,10 +65,26 @@ function IconSwitch({ticketType,}) {
 
 
 const dummy_group_members = [
-    {email:'will.m.pattison@gmail.com'},
-    {email:'dan@gmail.com'},
-    {email:'kristine@yahoo.com'},
-    {email:'danielle@hotmail.com'}
+    {
+        email:'will.m.pattison@gmail.com',
+        username:'williampattison',
+        _id:1
+    },
+    {
+        email:'dan@gmail.com',
+        username:'dantheman',
+        _id:2
+    },
+    {
+        email:'kristine@yahoo.com',
+        username:'kristineisbest123',
+        _id: 3
+    },
+    {
+        email:'danielle@hotmail.com',
+        username:'daniellehello2023',
+        _id:5
+    }
 ];
 
 
@@ -78,113 +100,151 @@ export default function ViewTicket() {
 
     //number of comments to be paginated.
     const comments_pagination = 5;
-
     const { ticketId } = useParams();
-
     const [ticketInfo, setTicketInfo] = useState({
         ticketType:'bug',
         briefDescription:'Sit fugiat aliquip est quis consectetur laboris sint excepteur do laboris aute enim. Laborum ullamco reprehenderit occaecat laborum reprehenderit eu irure est velit culpa. Laborum in dolor nostrud fugiat anim cupidatat consectetur laboris ex cillum anim minim. Ex elit officia est culpa excepteur irure. Elit est nostrud nostrud adipisicing Lorem officia nisi aliquip ipsum velit.',
         summary:'React application keeps crashing for no reason at all',
         priority:'high',
-        assignTo: {
-            email:'will.m.pattison@gmail.com',
-            username: 'william',
-            id:1,
-        },
+        assignTo: dummy_group_members[0].email,
         status:'in progress',
         createdOn:'02/26/1999',
         lastUpdated: '03/14/1999'
     });
 
+    /* STATE FOR HANDLING COMMENTS */
+
     //set state for ticket comments
     const [ticketComments, setTicketComments] = useState([1,2,3,4,5]);
-
 
     /* State for handling comments */
     const [searchComment, setSearchComment] = useState('');
     const debouncedCommentSearch = useDebounce(searchComment,1000);
-
     const [commentFilters, setCommentFilters] = useState({});
+    const [commentMenuRef, setCommentMenuRef] = useState(null);
+    const showCommentMenu = Boolean(commentMenuRef);
+    const [createCommentMode, setCreateCommentMode] = useState(false);
+
+    /* Create async request to server adding a new comment to the list.
+    * make a new request to the server with the updated comments list to show
+    * the most recent changes. 
+    */
+    function onCreateNewComment(newComment) {
+        console.log('Creating comment',newComment);
+        onOpenSnackbar("Comment successfully created!", "success");
+    }
 
     function handleCommentFilterChange(e) {
         // setCommentFilters(e.target.value);
     }
 
-    const [commentMenuRef, setCommentMenuRef] = useState(null);
-    const showCommentMenu = Boolean(commentMenuRef);
-
-    const [createCommentMode, setCreateCommentMode] = useState(false);
-
     function onOpenCommentMenu(e) {
         setCommentMenuRef(e.currentTarget);
     }
 
-    function onCloseMenu(){
+    function onCloseCommentMenu(){
         setCommentMenuRef(null);
     }
 
+    /**********END COMMENT STATE *******************/
 
-    /* For handling snackbar state */
+
+
+    /**********STATE FOR SNACKBAR ************/
+    /* Conditionally show snackbar */
     const [openSnackbar, setOpenSnackbar] = useState(false);
+    /* State for holding the snackbar message to be displayed to the user. */
     const [snackbarMessage, setSnackbarMessage] = useState('');
-
+    /*Change the color of the snackbar for either an error message or success*/
+    const [snackbarSeverity, setSnackbarSeverity] = useState('success');
 
     function onCloseSnackbar() {
         setOpenSnackbar(false);
     }
 
+    /*****END STATE FOR SNACKBAR ************/
 
+    /* IN EDIT MODE */
     /* For edit mode in ticket */
     const [canEdit, setCanEdit] = useState(false);
-    const [loadingPublishChanges, setLoadingPublishChanges] = useState(false);
-
+    /* Condition to display the button to publish changes. */
     const [displayPublishChangesButton, setDisplayPublishChangesButton] = useState(false);
-
+    /* show pending state when ticket update is sent. */
+    const [pendingPublishChanges, setPendingPublishChanges] = useState(false);
     //store deep copy of original ticket.
     const [ticketInfoCopy, setTicketInfoCopy] = useState(_.cloneDeep(ticketInfo));
+    /* Modal that is displayed before updates are applied to ticket. */
+    const [showConfirmTicketChangesModal, setShowConfirmTicketChangesModal] = useState(false);
+    /* Hold the ticket comments that user adds before submitting the changes. */
+    const [ticketChangeComments, setTicketChangeComments] = useState('');
 
+    function onDisplayConfirmTicketModal() {
+        setShowConfirmTicketChangesModal(true);
+        setPendingPublishChanges(false);
+    }
 
-   
+    /* self explanatory, wrapper function. */
+    function handleTicketChangeComments(event) {
+        setTicketChangeComments(event.target.value);
+    }
+
+    function onHandleEditSwitchToggle() {
+        if(canEdit){
+            setTicketInfoCopy(_.cloneDeep(ticketInfo));
+            setDisplayPublishChangesButton(false);
+        }
+        setCanEdit((canEdit)=> !canEdit);
+    }
+    /***************** */
+    /* Revert all ticket changes and disable edit mode and its state features. */
+    function onDiscardTicketChanges(){
+        setShowConfirmTicketChangesModal(false);
+        setPendingPublishChanges(false);
+        setCanEdit(false);
+        setTicketInfoCopy(_.cloneDeep(ticketInfo));
+        setDisplayPublishChangesButton(false);
+        setTicketChangeComments('');
+        onOpenSnackbar("Ticket modifications have been discarded","error");
+        
+    }
 
     /* Whenever makes modification to a field,
     display a button on screen to allow modifications. */
     function updateTicketCopy(newValue, field) {
-
-        const modifiedTicketInfoCopy = {
-            ...ticketInfoCopy,
-            [field]:newValue
-        };
-
+        const modifiedTicketInfoCopy = {...ticketInfoCopy,[field]:newValue};
         /* Deep comparison of objects */
         const madeTicketModifications = !_.isEqual(ticketInfo, modifiedTicketInfoCopy);
 
+        console.log('newvalue,modified,real',newValue,modifiedTicketInfoCopy, ticketInfoCopy);
         /* if modifications have been made, allow users to publish changes. */
         if(madeTicketModifications)
             setDisplayPublishChangesButton(true);
         else
             setDisplayPublishChangesButton(false);
-
         setTicketInfoCopy(modifiedTicketInfoCopy);
     }
 
+
+    /* Perform update on entire ticket. */
     function onSubmitEditChanges() {
-        setLoadingPublishChanges(true);
+        setPendingPublishChanges(true);
+
+        console.log('setting loading')
+
+        //simulate the changes being made.
         const timeout = setTimeout(()=> {
-            setLoadingPublishChanges(false);
+            setShowConfirmTicketChangesModal(false);
             setTicketInfo(_.cloneDeep(ticketInfoCopy));
             setDisplayPublishChangesButton(false);
+            setCanEdit(false);
+            setTicketChangeComments('');
+            onOpenSnackbar("Successfully updated ticket.","success");
         },2000);
-
-
         //just update the ticket manually, make the change later from the db.
-
-
         //submit the new ticket,
         //fetch the newly updated ticket and store it in the original ticket.
         //append the change to the ticket history (this can be handled by the backend.)
     }
-
-    /******* */
 
 
     /**
@@ -195,31 +255,22 @@ export default function ViewTicket() {
     function onOpenSnackbar(message, severity) {
         setSnackbarMessage(message);
         setOpenSnackbar(true);
+        setSnackbarSeverity(severity);
     }
 
-
-    /* Create async request to server adding a new comment to the list.
-    * make a new request to the server with the updated comments list to show
-    * the most recent changes. 
-    */
-    function onCreateNewComment(newComment) {
-        console.log('Creating comment',newComment);
-        onOpenSnackbar("Comment successfully created!");
-    }
 
     /* **** */
 
-    //get pagination for the number of comments
-
-
     useEffect(()=> {
         //async request to get the ticket information using the ticketId params.
-
-        //get the comments associated with the ticket, make sure to include pagination
-        //to the results
+        //get the comments associated with the ticket, make sure to include pagination to the results.
     },[]);
 
 
+    /**
+     * @param {string} priority - can be either high | medium | low and maps those accordingly
+     * @returns string - the mapped priority for the MUI Chip component
+     */
     function getPriorityColor(priority) {
         switch(priority){
             case "high":
@@ -228,21 +279,23 @@ export default function ViewTicket() {
                 return "warning";
             case "low":
                 return "success";
+            default:
+                return "success"
         }
     }
 
-    console.log(ticketId);
 
 
     return (
         <div className="vt-container">
             {
                 canEdit && displayPublishChangesButton && (
-                <div className="vt-submit-changes-container">
+                <div className={`vt-submit-changes-container`}>
+                
                     <button
-                        className="vt-submit-changes"
-                        onClick={onSubmitEditChanges}
-                        disabled={loadingPublishChanges}
+                        className={`vt-submit-changes  ${ showConfirmTicketChangesModal?'vt-submit-disabled':''}`}
+                        onClick={onDisplayConfirmTicketModal}
+                        disabled={showConfirmTicketChangesModal}
                     >
                         Publish Changes
                         <Publish
@@ -250,16 +303,15 @@ export default function ViewTicket() {
                         />
                     </button>
                     {
-                        loadingPublishChanges && (
+                        showConfirmTicketChangesModal && (
                             <CircularProgress
-                                variant="determinant"
                                 color="success"
-                                size={'small'}
+                                size={'1.5rem'}
                                 sx={{
                                     position:'absolute',
-                                    top:'50%',
-                                    left:'50%',
-                                    margin:'10px 0 0 10px'
+                                    top:'-2px',
+                                    left:'40%',
+                                    margin:'10px 0 0 10px',
                                 }}
                             />
                         )
@@ -273,14 +325,14 @@ export default function ViewTicket() {
                 onClose={onCloseSnackbar}
                 anchorOrigin={{horizontal:'center', vertical:'bottom'}}
             >
-                <Alert severity="success">
+                <Alert severity={snackbarSeverity}>
                     {snackbarMessage}
                 </Alert>
             </Snackbar>
             <Menu
                 anchorEl={commentMenuRef}
                 open={showCommentMenu}
-                onClose={onCloseMenu}
+                onClose={onCloseCommentMenu}
             >
                 <Box sx={{padding:'0 10px 10px 10px'}}>
                     <Typography fontSize={'0.9rem'}>Apply Search Filters</Typography>
@@ -320,6 +372,79 @@ export default function ViewTicket() {
                 />
                 </MenuItem>
             </Menu>
+            {/* Dialog for submitting ticket changes. */}
+            <Dialog
+                open={showConfirmTicketChangesModal}
+                onClose={()=> null}
+            >
+                <DialogTitle>{'Please briefly describe your changes.'}</DialogTitle>
+                <DialogContent>
+                    {
+                        (()=> {
+                            if(pendingPublishChanges){
+                                return (
+                                    <Stack direction={'column'} alignItems={"center"} justifyContent={'center'}>
+                                        <CircularProgress size={'2rem'}/>
+                                        <DialogContentText textAlign={'center'}>Publishing changes...</DialogContentText>
+                                    </Stack>
+                                );
+                            }
+                            else {
+                                return (
+                                    <div style={{position:'relative'}}>
+                                        <textarea
+                                            value={ticketChangeComments}
+                                            onChange={handleTicketChangeComments}
+                                            className="vt-ticket-change-textarea"
+                                            placeholder="Note: keep your description only 1 to 2 sentences."
+                                            maxLength={100}
+                                        >
+                                        </textarea>
+                                        <span 
+                                            style={{
+                                                color:'gray', 
+                                                position:'absolute', 
+                                                right:'0', 
+                                                bottom:'-5px',
+                                                fontSize:'0.7rem'
+                                            }} 
+                                            className="text"
+                                        >
+                                            {`${ticketChangeComments.length}/100 characters`}
+                                        </span>
+                                    </div>
+                                );
+                            }
+                        })()
+                    }
+                </DialogContent>
+                <DialogActions sx={{justifyContent:'center'}}>
+                    {
+                        (()=> {
+                            //give user to cancel or apply ticket changes before submission.
+                            if(!pendingPublishChanges){
+                                return (
+                                    <>
+                                        <Button
+                                            onClick={onDiscardTicketChanges}
+                                            variant="contained"
+                                            color="error"
+                                            sx={{textTransform:'none'}}
+                                        >Discard</Button>
+                                        <Button
+                                            onClick={onSubmitEditChanges}
+                                            variant="contained"
+                                            color="info"
+                                            disabled={ticketChangeComments.length === 0}
+                                            sx={{textTransform:'none'}}
+                                        >Apply</Button>
+                                    </>
+                                );
+                            }
+                        })()
+                    }
+                </DialogActions>
+            </Dialog>
             <div className="vt-header">
                 <p className="text">
                     {ticketInfo.summary}
@@ -328,8 +453,9 @@ export default function ViewTicket() {
                     label={`Allow editing: ${canEdit?"on":'off'}`}
                     control={
                         <Switch
-                            onChange={()=> setCanEdit(!canEdit)}
+                            onChange={onHandleEditSwitchToggle}
                             value={canEdit}
+                            checked={canEdit}
                             disabled={false}//if user does not have permission, do not edit.
                         />
                     }
@@ -359,7 +485,7 @@ export default function ViewTicket() {
                                             canEdit?(
                                                 <td className="vt-td-value">
                                                     <select 
-                                                        onChange={(e)=> updateTicketCopy(e.target.value,"assignedTo")}
+                                                        onChange={(e)=> updateTicketCopy(e.target.value,"assignTo")}
                                                         value={ticketInfoCopy.assignTo}
                                                         className="vt-edit-select"
                                                     >
@@ -379,7 +505,7 @@ export default function ViewTicket() {
                                                 </td>
 
                                             ):(
-                                                <td className="vt-td-value">{ticketInfo.assignTo.email}</td>
+                                                <td className="vt-td-value">{ticketInfo.assignTo}</td>
                                             )
                                         }
                                     </tr>
@@ -444,8 +570,8 @@ export default function ViewTicket() {
                                             {
                                                 canEdit? (
                                                     <select 
-                                                        value={ticketInfoCopy.ticketType}
-                                                        onChange={(e)=> updateTicketCopy(e.target.value,"ticketType")}
+                                                        value={ticketInfoCopy.status}
+                                                        onChange={(e)=> updateTicketCopy(e.target.value,"status")}
                                                         className="vt-edit-select"
                                                     >
                                                         <option value="open">Open</option>
